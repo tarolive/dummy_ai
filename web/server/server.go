79b9db71e13d7b798aec1e29e9492c92fa7310db
Tarcisio Oliveira <tarolive@redhat.com>
2023-05-26 11:24:08 -0300

    update

--- web/server/server.go
+++ web/server/server.go
@@ -12,6 +12,11 @@ func Run() {
 
 	serveFile("/apple_touch_icon.png" /**/, "./web/static/image/favicon/apple_touch_icon.png")
 	serveFile("/favicon.ico" /*         */, "./web/static/image/favicon/favicon.ico")
+	serveFile("/favicon.svg" /*         */, "./web/static/image/favicon/favicon.svg")
+	serveFile("/favicon_192x192.png" /* */, "./web/static/image/favicon/favicon_192x192.png")
+	serveFile("/favicon_512x512.png" /* */, "./web/static/image/favicon/favicon_512x512.png")
+	serveFile("/logo.svg" /*            */, "./web/static/image/logo/logo.svg")
+	serveFile("/logo_white.svg" /*      */, "./web/static/image/logo/logo_white.svg")
 
 	if err := http.ListenAndServe(env.ServerAddress, nil); err != nil {
 
