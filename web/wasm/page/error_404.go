b0f1d4989d858185ab103c97fc0aacc431a3f0db
Tarcisio Oliveira <tarolive@redhat.com>
2023-06-20 16:09:58 -0300

    update

--- web/wasm/page/error_404.go
+++ web/wasm/page/error_404.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"syscall/js"
-)
-
-import (
 	"dummy_ai/web/wasm/layout"
 )
 
 func main() {
 
-	document := js.Global().Get("document")
-
-	h2 := document.Call("createElement", "h2")
+	h2 := layout.Document.Call("createElement", "h2")
 	h2.Set("innerHTML", "Error 404 - Page Not Found")
 
 	layout.NewPage(h2)
--- web/wasm/page/index.go
+++ web/wasm/page/index.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"syscall/js"
-)
-
-import (
 	"dummy_ai/web/wasm/layout"
 )
 
 func main() {
 
-	document := js.Global().Get("document")
-
-	h2 := document.Call("createElement", "h2")
+	h2 := layout.Document.Call("createElement", "h2")
 	h2.Set("innerHTML", "Hello World, DummyAI!")
 
 	layout.NewPage(h2)
