469113077cd6995c71156513906d363ab9d9c19e
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-01 19:34:06 -0300

    update

--- web/wasm/layout/page.go
+++ web/wasm/layout/page.go
@@ -19,8 +19,24 @@ var (
 
 func CreatePage(page js.Value) {
 
+	body.Call("appendChild", createPageContainer(page))
+}
+
+func createPageContainer(page js.Value) js.Value {
+
 	pageContainer := document.Call("createElement", "div")
 	pageContainer.Call("appendChild", page)
 
-	body.Call("appendChild", pageContainer)
+	pageContainerStyle := pageContainer.Get("style")
+	pageContainerStyle.Set("position", "fixed")
+	pageContainerStyle.Set("top", "0")
+	pageContainerStyle.Set("right", "0")
+	pageContainerStyle.Set("bottom", "0")
+	pageContainerStyle.Set("left", "0")
+	pageContainerStyle.Set("padding", "10px")
+	pageContainerStyle.Set("overflow-y", "auto")
+	pageContainerStyle.Set("background-color", "#151515")
+	pageContainerStyle.Set("color", "white")
+
+	return pageContainer
 }
