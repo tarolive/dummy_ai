010921b27aa761f8334743999117d54c3f18adb3
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-09 17:10:42 -0300

    update

--- web/wasm/layout/page.go
+++ web/wasm/layout/page.go
@@ -19,7 +19,24 @@ var (
 
 func CreatePage(page js.Value) {
 
+	body.Call("appendChild", createNavigation())
 	body.Call("appendChild", createPageContainer(page))
+}
+
+func createNavigation() js.Value {
+
+	navigation := document.Call("createElement", "div")
+
+	navigationStyle := navigation.Get("style")
+	navigationStyle.Set("position" /*         */, "fixed")
+	navigationStyle.Set("top" /*              */, "0")
+	navigationStyle.Set("right" /*            */, "0")
+	navigationStyle.Set("left" /*             */, "0")
+	navigationStyle.Set("height" /*           */, "72px")
+	navigationStyle.Set("background-color" /* */, "var(--rh-color-surface-darkest)")
+	navigationStyle.Set("color" /*            */, "var(--rh-color-text-primary-on-dark)")
+
+	return navigation
 }
 
 func createPageContainer(page js.Value) js.Value {
@@ -29,7 +46,7 @@ func createPageContainer(page js.Value) js.Value {
 
 	pageContainerStyle := pageContainer.Get("style")
 	pageContainerStyle.Set("position" /*         */, "fixed")
-	pageContainerStyle.Set("inset" /*            */, "0 0 0 0")
+	pageContainerStyle.Set("inset" /*            */, "72px 0 0 0")
 	pageContainerStyle.Set("padding" /*          */, "10px")
 	pageContainerStyle.Set("overflow-x" /*       */, "hidden")
 	pageContainerStyle.Set("overflow-y" /*       */, "auto")
