428b25af0c8ff3ef389028744b736fde9a10dd4f
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-13 15:03:37 -0300

    update

--- web/wasm/util/language.go
+++ web/wasm/util/language.go
@@ -1,6 +1,20 @@
 package util
 
+var (
+	language = ""
+)
+
 func Language() string {
+
+	if language != "" {
+
+		return language
+	}
+
+	if language = LocalStorage("language"); language != "" {
+
+		return language
+	}
 
 	return "en"
 }
