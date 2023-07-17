a9a332e120ac31016dbead12541bbbb3da6a1693
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-16 23:18:52 -0300

    update

--- web/wasm/template_util/language.go
+++ web/wasm/template_util/language.go
@@ -1,12 +1,26 @@
 package template_util
 
+import (
+	"syscall/js"
+)
+
 const (
 	ENGLISH    = "en"
 	SPANISH    = "es"
 	PORTUGUESE = "pt"
 )
 
+var (
+	_window       = js.Global()
+	_localStorage = _window.Get("localStorage")
+)
+
 func Language() string {
+
+	if language := _localStorage.Call("getItem", "language").String(); isSupported(language) {
+
+		return language
+	}
 
 	return ENGLISH
 }
