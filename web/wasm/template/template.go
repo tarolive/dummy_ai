52749745b73143145875d69af0a9d7a17f872279
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-18 15:24:07 -0300

    update

--- web/wasm/template/template.go
+++ web/wasm/template/template.go
@@ -1,9 +1,18 @@
 package template
 
 import (
+	"syscall/js"
+)
+
+import (
 	"dummy_ai/web/wasm/template_util"
 )
 
 var (
 	Language = template_util.Language()
+)
+
+var (
+	_window   = js.Global()
+	_document = _window.Get("document")
 )
