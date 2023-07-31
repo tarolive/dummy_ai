10fcf35ed604fa417da8642f247b7511d4641093
Tarcisio Oliveira <tarolive@redhat.com>
2023-07-31 02:38:51 -0300

    update

--- web/wasm/page/index.go
+++ web/wasm/page/index.go
@@ -60,6 +60,8 @@ func createSearchInput() js.Value {
 	searchInputStyle.Set("padding" /*      */, "var(--rh-space-sm) var(--rh-space-md)")
 	searchInputStyle.Set("font-family" /*  */, "var(--rh-font-family-body-text)")
 	searchInputStyle.Set("font-size" /*    */, "var(--rh-font-size-body-text-md)")
+	searchInputStyle.Set("border" /*       */, "var(--rh-border-width-sm) solid")
+	searchInputStyle.Set("border-color" /* */, "#f0f0f0 #f0f0f0 #8a8d90 #f0f0f0")
 
 	return searchInput
 }
