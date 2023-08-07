e71d73ccedf08e12e926addccefdb9fbb9a90cfc
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-07 02:23:00 -0300

    update

--- web/wasm/template_util/vars.go
+++ web/wasm/template_util/vars.go
@@ -5,15 +5,21 @@ import (
 )
 
 var (
-	Window         = js.Global()
-	Document       = Window.Get("document")
-	SessionStorage = Window.Get("sessionStorage")
-	LocalStorage   = Window.Get("localStorage")
-	Navigator      = Window.Get("navigator")
+	Window = js.Global()
 )
 
 var (
+	Document        = Window.Get("document")
 	DocumentElement = Document.Get("documentElement")
 	Head            = Document.Get("head")
 	Body            = Document.Get("body")
+)
+
+var (
+	SessionStorage = Window.Get("sessionStorage")
+	LocalStorage   = Window.Get("localStorage")
+)
+
+var (
+	Navigator = Window.Get("navigator")
 )
