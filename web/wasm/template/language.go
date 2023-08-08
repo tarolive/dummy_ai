a3eccf776dc3dced8ac639fd3092308a12f52473
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-08 02:19:59 -0300

    update

--- web/wasm/template/language.go
+++ web/wasm/template/language.go
@@ -19,3 +19,18 @@ func SetLanguage(language string) {
 
 	js.Global().Get("localStorage").Call("setItem", "language", language)
 }
+
+func isSupportedLanguage(language string) bool {
+
+	switch language {
+
+	case ENGLISH, SPANISH, PORTUGUESE:
+
+		return true
+
+	default:
+
+		return false
+
+	}
+}
