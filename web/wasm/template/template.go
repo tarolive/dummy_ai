406e70fd8b11751b5820ca35007e7e6431a2d880
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-14 19:27:57 -0300

    update

--- web/wasm/template/template.go
+++ web/wasm/template/template.go
@@ -5,84 +5,29 @@ import (
 )
 
 var (
-	page = createPage()
+	navigator     = createNavigator()
+	pageContainer = createPageContainer()
 )
 
 func init() {
-
-	html := js.Global().Get("document").Get("documentElement")
-	html.Set("lang", Language())
-
-	body := js.Global().Get("document").Get("body")
-	body.Call("appendChild", createNavigation())
-	body.Call("appendChild", page)
 }
 
-func Page() js.Value {
+func Navigator() js.Value {
 
-	return page
+	return navigator
 }
 
-func createPage() js.Value {
+func PageContainer() js.Value {
 
-	page := js.Global().Get("document").Call("createElement", "div")
-
-	pageStyle := page.Get("style")
-	pageStyle.Set("position" /*         */, "fixed")
-	pageStyle.Set("top" /*              */, "72px")
-	pageStyle.Set("right" /*            */, 0)
-	pageStyle.Set("bottom" /*           */, 0)
-	pageStyle.Set("left" /*             */, 0)
-	pageStyle.Set("padding" /*          */, "var(--rh-space-md)")
-	pageStyle.Set("overflow-x" /*       */, "hidden")
-	pageStyle.Set("overflow-y" /*       */, "auto")
-	pageStyle.Set("background-color" /* */, "var(--rh-color-surface-lightest)")
-	pageStyle.Set("color" /*            */, "var(--rh-color-text-primary-on-light)")
-
-	return page
+	return pageContainer
 }
 
-func createNavigation() js.Value {
+func createNavigator() js.Value {
 
-	navigation := js.Global().Get("document").Call("createElement", "div")
-	navigation.Call("appendChild", createNavigationImage())
-	navigation.Call("appendChild", createNavigationText())
-
-	navigationStyle := navigation.Get("style")
-	navigationStyle.Set("position" /*         */, "fixed")
-	navigationStyle.Set("top" /*              */, 0)
-	navigationStyle.Set("right" /*            */, 0)
-	navigationStyle.Set("left" /*             */, 0)
-	navigationStyle.Set("height" /*           */, "72px")
-	navigationStyle.Set("padding" /*          */, "0 var(--rh-space-xl)")
-	navigationStyle.Set("display" /*          */, "flex")
-	navigationStyle.Set("align-items" /*      */, "center")
-	navigationStyle.Set("background-color" /* */, "var(--rh-color-surface-darkest)")
-	navigationStyle.Set("color" /*            */, "var(--rh-color-text-primary-on-dark)")
-
-	return navigation
+	return js.Value{}
 }
 
-func createNavigationImage() js.Value {
+func createPageContainer() js.Value {
 
-	navigationImage := js.Global().Get("document").Call("createElement", "img")
-	navigationImage.Set("src", "/logo.svg")
-	navigationImage.Set("alt", "")
-
-	navigationImageStyle := navigationImage.Get("style")
-	navigationImageStyle.Set("width" /*  */, "var(--rh-space-2xl)")
-	navigationImageStyle.Set("height" /* */, "var(--rh-space-2xl)")
-
-	return navigationImage
-}
-
-func createNavigationText() js.Value {
-
-	navigationText := js.Global().Get("document").Call("createElement", "h2")
-	navigationText.Set("innerHTML", "DummyAI")
-
-	navigationTextStyle := navigationText.Get("style")
-	navigationTextStyle.Set("margin" /* */, "0 var(--rh-space-xl)")
-
-	return navigationText
+	return js.Value{}
 }
