3e06a8ab9b460b056c81410c07f1407e580bfcb7
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-21 23:17:34 -0300

    update

--- web/wasm/template/responsive.go
+++ web/wasm/template/responsive.go
@@ -1,1 +1,25 @@
 package template
+
+import (
+	"syscall/js"
+)
+
+var (
+	media      = js.Global().Call("getComputedStyle", js.Global().Get("document").Get("documentElement")).Call("getPropertyValue", "--rh-media-sm")
+	matchMedia = js.Global().Call("matchMedia", media)
+)
+
+var (
+	isDesktop = matchMedia.Get("matches").Bool()
+	isMobile  = !isDesktop
+)
+
+func IsDesktop() bool {
+
+	return isDesktop
+}
+
+func IsMobile() bool {
+
+	return isMobile
+}
