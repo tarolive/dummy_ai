6babe52a4a269eec896e9102efba7670d4ebe5e3
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-28 22:39:43 -0300

    update

--- web/wasm/page/index.go
+++ web/wasm/page/index.go
@@ -13,7 +13,7 @@ func main() {
 	h2 := js.Global().Get("document").Call("createElement", "h2")
 	h2.Set("innerHTML", "Index")
 
-	template.Page().Call("appendChild", h2)
+	template.PageMainSection().Call("appendChild", h2)
 
 	select {}
 }
