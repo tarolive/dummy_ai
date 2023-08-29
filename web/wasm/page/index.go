4da5b6db691a8195e17173f2fb06722c588d5ec2
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-28 22:09:15 -0300

    update

--- web/wasm/page/index.go
+++ web/wasm/page/index.go
@@ -8,64 +8,12 @@ import (
 	"dummy_ai/web/wasm/template"
 )
 
-var (
-	_messages = map[string]map[string]string{
-		template.English: {
-			"searchButton.innerHTML":  "Search",
-			"searchInput.placeholder": "What are you looking for?",
-		},
-		template.Spanish: {
-			"searchButton.innerHTML":  "Buscar",
-			"searchInput.placeholder": "¿Qué estás buscando?",
-		},
-		template.Portuguese: {
-			"searchButton.innerHTML":  "Buscar",
-			"searchInput.placeholder": "O que você está buscando?",
-		},
-	}[template.Language()]
-)
-
 func main() {
 
-	template.Page().Call("appendChild", _createSearchBar())
+	h2 := js.Global().Get("document").Call("createElement", "h2")
+	h2.Set("innerHTML", "Index")
+
+	template.Page().Call("appendChild", h2)
+
 	select {}
-}
-
-func _createSearchBar() js.Value {
-
-	searchBar := js.Global().Get("document").Call("createElement", "div")
-	searchBar.Call("appendChild", _createSearchBarInput())
-	searchBar.Call("appendChild", _createSearchBarButton())
-
-	searchBarStyle := searchBar.Get("style")
-	searchBarStyle.Set("display" /* */, "flex")
-	searchBarStyle.Set("gap" /*     */, "var(--rh-space-md)")
-
-	return searchBar
-}
-
-func _createSearchBarInput() js.Value {
-
-	searchInput := js.Global().Get("document").Call("createElement", "input")
-	searchInput.Set("type", "search")
-	searchInput.Set("placeholder", _messages["searchInput.placeholder"])
-
-	searchInputStyle := searchInput.Get("style")
-	searchInputStyle.Set("flex" /*         */, 1)
-	searchInputStyle.Set("padding" /*      */, "var(--rh-space-sm) var(--rh-space-md)")
-	searchInputStyle.Set("font-family" /*  */, "var(--rh-font-family-body-text)")
-	searchInputStyle.Set("font-size" /*    */, "var(--rh-font-size-body-text-md)")
-	searchInputStyle.Set("border-style" /* */, "solid")
-	searchInputStyle.Set("border-width" /* */, "var(--rh-border-width-sm)")
-	searchInputStyle.Set("border-color" /* */, "#f0f0f0 #f0f0f0 #8a8d90 #f0f0f0")
-
-	return searchInput
-}
-
-func _createSearchBarButton() js.Value {
-
-	searchButton := js.Global().Get("document").Call("createElement", "rh-button")
-	searchButton.Set("innerHTML", _messages["searchButton.innerHTML"])
-
-	return searchButton
 }
