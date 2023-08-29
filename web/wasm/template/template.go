ef676bbd625a5eea30b280691b71a92dd6e94315
Tarcisio Oliveira <tarolive@redhat.com>
2023-08-28 22:18:17 -0300

    update

--- web/wasm/template/template.go
+++ web/wasm/template/template.go
@@ -5,29 +5,13 @@ import (
 )
 
 var (
-	isExpandedDrawer = false
-)
-
-var (
-	page                 = createPage()
-	navigationMenuButton = createNavigationMenuButton()
-	navigationTitleImage = createNavigationTitleImage()
-	navigationTitle      = createNavigationTitle()
-	navigation           = createNavigation()
-	drawer               = createDrawer()
+	page = createPage()
 )
 
 func init() {
 
 	js.Global().Get("document").Get("documentElement").Set("lang", language)
 	js.Global().Get("document").Get("body").Call("appendChild", page)
-	js.Global().Get("document").Get("body").Call("appendChild", navigation)
-	js.Global().Get("document").Get("body").Call("appendChild", drawer)
-}
-
-func IsExpandedDrawer() bool {
-
-	return isExpandedDrawer
 }
 
 func Page() js.Value {
@@ -35,172 +19,8 @@ func Page() js.Value {
 	return page
 }
 
-func NavigationMenuButton() js.Value {
-
-	return navigationMenuButton
-}
-
-func NavigationTitleImage() js.Value {
-
-	return navigationTitleImage
-}
-
-func NavigationTitle() js.Value {
-
-	return navigationTitle
-}
-
-func Navigation() js.Value {
-
-	return navigation
-}
-
-func Drawer() js.Value {
-
-	return drawer
-}
-
-func ToggleDrawer() {
-
-	isExpandedDrawer = !isExpandedDrawer
-
-	if isExpandedDrawer {
-
-		drawer.Get("style").Set("transform" /* */, "translateX(0)")
-	}
-
-	if !isExpandedDrawer {
-
-		drawer.Get("style").Set("transform" /* */, "translateX(-100%)")
-	}
-}
-
-func createNavigation() js.Value {
-
-	navigation := js.Global().Get("document").Call("createElement", "div")
-	navigation.Call("appendChild", navigationTitleImage)
-	navigation.Call("appendChild", navigationTitle)
-	navigation.Get("style").Set("position" /*         */, "fixed")
-	navigation.Get("style").Set("top" /*              */, 0)
-	navigation.Get("style").Set("right" /*            */, 0)
-	navigation.Get("style").Set("left" /*             */, 0)
-	navigation.Get("style").Set("height" /*           */, "72px")
-	navigation.Get("style").Set("display" /*          */, "flex")
-	navigation.Get("style").Set("align-items" /*      */, "center")
-	navigation.Get("style").Set("background-color" /* */, "var(--rh-color-surface-darkest)")
-	navigation.Get("style").Set("color" /*            */, "var(--rh-color-text-primary-on-dark)")
-
-	if isDesktop {
-
-		navigation.Get("style").Set("padding-left" /* */, "var(--rh-space-xl)")
-	}
-
-	if isMobile {
-
-		navigation.Get("style").Set("padding-left" /* */, "var(--rh-space-xs)")
-		navigation.Call("insertBefore", navigationMenuButton, navigationTitleImage)
-	}
-
-	return navigation
-}
-
-func createNavigationMenuButton() js.Value {
-
-	img := js.Global().Get("document").Call("createElement", "img")
-	img.Set("src", "/menu_open_white.svg")
-	img.Set("alt", messages[Menu])
-	img.Get("style").Set("width" /*  */, "var(--rh-size-icon-02)")
-	img.Get("style").Set("height" /* */, "var(--rh-size-icon-02)")
-
-	div := js.Global().Get("document").Call("createElement", "div")
-	div.Call("appendChild", img)
-	div.Get("style").Set("width" /*  */, "var(--rh-size-icon-02)")
-	div.Get("style").Set("height" /* */, "var(--rh-size-icon-02)")
-	div.Get("style").Set("margin" /* */, "var(--rh-space-sm) 0")
-
-	navigationMenuButton := js.Global().Get("document").Call("createElement", "rh-button")
-	navigationMenuButton.Set("variant", "link")
-	navigationMenuButton.Call("appendChild", div)
-
-	navigationMenuButton.Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) any {
-
-		ToggleDrawer()
-		return nil
-	}))
-
-	return navigationMenuButton
-}
-
-func createNavigationTitleImage() js.Value {
-
-	navigationTitleImage := js.Global().Get("document").Call("createElement", "img")
-	navigationTitleImage.Set("src", "/logo.svg")
-	navigationTitleImage.Set("alt", "")
-	navigationTitleImage.Get("style").Set("width" /*        */, "var(--rh-size-icon-02)")
-	navigationTitleImage.Get("style").Set("height" /*       */, "var(--rh-size-icon-02)")
-	navigationTitleImage.Get("style").Set("margin-right" /* */, "var(--rh-space-md)")
-
-	if isMobile {
-
-		navigationTitleImage.Get("style").Set("margin-left" /* */, "var(--rh-space-sm)")
-	}
-
-	return navigationTitleImage
-}
-
-func createNavigationTitle() js.Value {
-
-	navigationTitle := js.Global().Get("document").Call("createElement", "h3")
-	navigationTitle.Set("innerHTML", messages[DummyAI])
-
-	return navigationTitle
-}
-
-func createDrawer() js.Value {
-
-	drawer := js.Global().Get("document").Call("createElement", "div")
-	drawer.Get("style").Set("position" /*         */, "fixed")
-	drawer.Get("style").Set("top" /*              */, "72px")
-	drawer.Get("style").Set("bottom" /*           */, 0)
-	drawer.Get("style").Set("left" /*             */, 0)
-	drawer.Get("style").Set("width" /*            */, "100%")
-	drawer.Get("style").Set("max-width" /*        */, "290px")
-	drawer.Get("style").Set("overflow-x" /*       */, "hidden")
-	drawer.Get("style").Set("overflow-y" /*       */, "auto")
-	drawer.Get("style").Set("background-color" /* */, "#212427")
-	drawer.Get("style").Set("color" /*            */, "var(--rh-color-text-primary-on-dark)")
-	drawer.Get("style").Set("z-index" /*          */, 10)
-
-	if isMobile {
-
-		drawer.Get("style").Set("transform" /* */, "translateX(-100%)")
-	}
-
-	return drawer
-}
-
 func createPage() js.Value {
 
 	page := js.Global().Get("document").Call("createElement", "div")
-	page.Get("style").Set("position" /*         */, "fixed")
-	page.Get("style").Set("top" /*              */, "72px")
-	page.Get("style").Set("right" /*            */, 0)
-	page.Get("style").Set("bottom" /*           */, 0)
-	page.Get("style").Set("padding" /*          */, "var(--rh-space-md)")
-	page.Get("style").Set("overflow-x" /*       */, "hidden")
-	page.Get("style").Set("overflow-y" /*       */, "auto")
-	page.Get("style").Set("background-color" /* */, "var(--rh-color-surface-lightest)")
-	page.Get("style").Set("color" /*            */, "var(--rh-color-text-primary-on-light)")
-
-	if isDesktop {
-
-		page.Get("style").Set("left" /* */, "290px")
-	}
-
-	if isMobile {
-
-		page.Get("style").Set("left" /* */, 0)
-	}
-
 	return page
 }
