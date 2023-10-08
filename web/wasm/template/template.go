5eb32c6ea3040a1814e59f6496ce7530df8ecb52
Tarcisio Oliveira <tarolive@redhat.com>
2023-10-07 22:57:20 -0300

    update

--- web/wasm/template/template.go
+++ web/wasm/template/template.go
@@ -95,6 +95,7 @@ func ToggleSidebar() {
 
 	sidebar.Get("classList").Call("toggle", "pf-m-expanded")
 	backdrop.Get("classList").Call("toggle", "pf-v5-u-display-none")
+
 	js.Global().Get("document").Get("body").Get("classList").Call("toggle", "pf-v5-c-backdrop__open")
 }
 
